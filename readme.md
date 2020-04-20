1a232075292264f0c919440cfdd3e25e53d7d7ce
Artsiom Yandulski <a.yandulski@godeltech.com>
2020-04-20 18:45:03 +0300

    Fix issue with dissapearing traces

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 ## Telemetry PoC with Jaeger and Elasticsearch
 
-* first
-* second
+### PoC setup
+
+* Download elsaticsearch binaries (https://www.elastic.co/fr/downloads/elasticsearch) and extract them into \elasticsearch folder.
+* Execute script [run-all-in-one-with-es.ps1](https://github.com/a-yandulski/TelemetryPoC/blob/master/run-all-in-one-with-es.ps1) to run Elasticsearch and Jaeger components (Agent, Collector, Query).
+* Navigate to http://localhost:16686/ in the browser to view Jaeger UI.
--- run-all-in-one-with-es.ps1
+++ run-all-in-one-with-es.ps1
@@ -5,20 +5,45 @@ $esPassword = "PASSWORD"
 
 $env:SPAN_STORAGE_TYPE="elasticsearch"
 
+Write-Host "Starting Elasticsearch server at $esServer"
+
 Start-Process "$root\elasticsearch\bin\elasticsearch.bat" -WorkingDirectory "$root\elasticsearch\bin\"
 
 Start-Sleep 20
 
-$args = "--es.server-urls=$esServer --es.username=$esUserName --es.password=$esPassword --collector.zipkin.http-port=9411"
+Write-Host "Starting Jaeger-Collector"
+
+$args = "--es.server-urls=$esServer --es.username=$esUserName --es.password=$esPassword --collector.zipkin.http-port=9411 --es-archive.use-aliases=true --es.use-aliases=true"
 
 Start-Process -FilePath "$root\jaeger-collector.exe" -ArgumentList $args -WorkingDirectory "$root"
 
 Start-Sleep 5
 
+Write-Host "Starting Jaeger-Agent"
+
 $args = "--reporter.grpc.host-port=localhost:14250"
 
 Start-Process -FilePath "$root\jaeger-agent.exe" -ArgumentList $args -WorkingDirectory "$root"
 
-$args = "--es.server-urls=$esServer --es.username=$esUserName --es.password=$esPassword --query.ui-config=$root\ui-config.json"
+Write-Host "Updating dependencies index"
+
+$index = "jaeger-dependencies-2020-04-13"
+$date = Get-Date -Format o
+$body = @{
+    script=@{
+       source="ctx._source.timestamp = '$date'";
+       lang="painless"
+    }
+ }
+
+$json = $body | ConvertTo-Json
+
+Invoke-WebRequest -Uri "$esServer/$index/_update_by_query?conflicts=proceed" -Method "POST" -Body $json -ContentType "application/json"
+
+Write-Host "Starting Jaeger-Query"
+
+$args = "--es.server-urls=$esServer --es.username=$esUserName --es.password=$esPassword --query.ui-config=$root\ui-config.json --es-archive.use-aliases=true --es.use-aliases=true"
 
 Start-Process -FilePath "$root\jaeger-query.exe" -ArgumentList $args -WorkingDirectory "$root"
+
+Write-Host "All components started"
